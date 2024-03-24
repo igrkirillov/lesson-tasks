ec1bb8875093663102046fb225781fb5c3604687
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-03-25 01:33:28 +0300

    4 commit

--- src/js/TaskInputWidget.js
+++ src/js/TaskInputWidget.js
@@ -17,7 +17,12 @@ export default class TaskInputWidget {
   }
 
   onKeyDown(event) {
+    this.clearErrorEmpty();
     if (event.key === "Enter") {
+      if (this.isFieldEmpty()) {
+        this.setErrorEmpty();
+        return;
+      }
       this.tasksWidget.addNewTask(this.element.value);
       this.clearInput();
       this.tasksWidget.filter("");
@@ -26,8 +31,20 @@ export default class TaskInputWidget {
     }
   }
 
+  isFieldEmpty() {
+    return !this.element.value;
+  }
+
+  setErrorEmpty() {
+    this.element.closest("label").classList.add("error-empty");
+  }
+
+  clearErrorEmpty() {
+    this.element.closest("label").classList.remove("error-empty");
+  }
+
   onInput(event) {
-    console.log("input");
+    this.clearErrorEmpty();
     this.filterExecutor.execFilter(this.element.value);
   }
 
